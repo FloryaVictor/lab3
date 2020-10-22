e831c28afe332a997fb8ae965b7473243a3c8f02
FloryaVictor <lapunixa@yandex.ru>
2020-10-23 01:28:55 +0300

    Scripted auto-commit on change (2020-10-23 01:28:55) by gitwatch.sh

--- src/main/java/lab3/FlightsData.java
+++ src/main/java/lab3/FlightsData.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 
 public class FlightsData implements Serializable{
     public String[] columns;
-
+    final public static int delayIndex = 18;
+    final public static int isCanceled
     public FlightsData(String data){
         this.columns = StringTools.splitCSV(data);
     }
