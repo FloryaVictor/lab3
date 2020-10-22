db7a60877fd29dd43cdc7d7060511e497f761089
FloryaVictor <lapunixa@yandex.ru>
2020-10-23 00:48:01 +0300

    Scripted auto-commit on change (2020-10-23 00:48:01) by gitwatch.sh

--- src/main/java/lab3/AirportData.java
+++ src/main/java/lab3/AirportData.java
@@ -1,7 +1,12 @@
 package lab3;
 
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class AirportData implements Serializable{
+    @Override
+    private void writeObject(ObjectOutputStream out)
+    {
 
+    }
 }
