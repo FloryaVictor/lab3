df0af5d37d0bb9ac4d5a65b1741adb12d1e1c7e9
FloryaVictor <lapunixa@yandex.ru>
2020-10-23 00:55:23 +0300

    Scripted auto-commit on change (2020-10-23 00:55:23) by gitwatch.sh

--- src/main/java/lab3/AirportData.java
+++ src/main/java/lab3/AirportData.java
@@ -1,14 +1,20 @@
 package lab3;
 
 import java.io.IOException;
+import java.io.ObjectStreamException;
 import java.io.Serializable;
 
 import java.io.ObjectOutputStream;
 
 public class AirportData implements Serializable {
-    @Override
-    private void writeObject(ObjectOutputStream out) throws IOException
-    {
-        
+
+    private void writeObject(java.io.ObjectOutputStream out) throws IOException{
+
+    }
+    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException{
+
+    }
+    private void readObjectNoData() throws ObjectStreamException{
+
     }
 }
