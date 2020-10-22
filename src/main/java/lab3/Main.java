e769428be3404fedbed39ccd4252bfe1b45f5cea
FloryaVictor <lapunixa@yandex.ru>
2020-10-23 00:08:09 +0300

    Scripted auto-commit on change (2020-10-23 00:08:09) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -14,6 +14,7 @@ public class Main {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flights = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaRDD<String> airports = sc.textFile("")
+        JavaRDD<String> airports = sc.textFile("L_AIRPORT_ID.csv");
+        
     }
 }
