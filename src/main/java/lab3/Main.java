5c207e04829aee1595312b7d0de006e377b1d78c
FloryaVictor <lapunixa@yandex.ru>
2020-10-22 23:14:26 +0300

    Scripted auto-commit on change (2020-10-22 23:14:26) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -3,6 +3,10 @@ package lab3;
 public class Main {
     public static void main(String[] args) throws Exception
     {
+        if (args.length != 3) {
+            System.err.println("Usage: lab3.Main <input path table> <input path dict> <output path>");
+            System.exit(-1);
+        }
         
     }
 }
