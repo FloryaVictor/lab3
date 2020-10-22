2cef507ad1b34a6ed501103588b1f45d0dad7a4b
FloryaVictor <lapunixa@yandex.ru>
2020-10-22 23:35:04 +0300

    Scripted auto-commit on change (2020-10-22 23:35:04) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -1,5 +1,8 @@
 package lab3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class Main {
     public static void main(String[] args) throws Exception
     {
@@ -7,6 +10,8 @@ public class Main {
             System.err.println("Usage: lab3.Main <input path table> <input path dict> <output path>");
             System.exit(-1);
         }
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
         
     }
 }
