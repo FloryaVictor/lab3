cc46e9a73f0a2334854e41d5e8dadb255fd39dce
FloryaVictor <lapunixa@yandex.ru>
2020-10-23 01:02:45 +0300

    Scripted auto-commit on change (2020-10-23 01:02:45) by gitwatch.sh

--- src/main/java/lab3/AirportData.java
+++ src/main/java/lab3/AirportData.java
@@ -1,10 +1,7 @@
 package lab3;
 
-import java.io.IOException;
-import java.io.ObjectStreamException;
-import java.io.Serializable;
 
-import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 public class AirportData implements Serializable {
     public String code, name;
@@ -12,11 +9,5 @@ public class AirportData implements Serializable {
     {
         this.code = code;
         this.name = name;
-    }
-    private void writeObject(java.io.ObjectOutputStream out) throws IOException{
-        out.writeObject(this);
-    }
-    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException{
-        in.readObject(AirportData.class);
     }
 }
--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -4,7 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import java.io.Serializable;
+
 
 public class Main {
     public static void main(String[] args) throws Exception
