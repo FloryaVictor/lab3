d44aa9eccd7c9a528f6656ddcae42f15ce6254cb
FloryaVictor <lapunixa@yandex.ru>
2020-10-23 03:23:39 +0300

    Scripted auto-commit on change (2020-10-23 03:23:39) by gitwatch.sh

--- src/main/java/lab3/StringTools.java
+++ src/main/java/lab3/StringTools.java
@@ -3,10 +3,10 @@ package lab3;
 public class StringTools {
     private final static String csvDelimiter = ",";
     public static String[] splitCSV(String csvLine){
-        return csvLine.split(csvDelimiter);
+        return removeQuotes(csvLine).split(csvDelimiter);
     }
     public static String removeQuotes(String s)
     {
-        
+        return s.replaceAll("\"","");
     }
 }
