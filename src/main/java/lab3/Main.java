383febacd61b4ba5d44a5d7efb239f82f01deb5e
FloryaVictor <lapunixa@yandex.ru>
2020-10-23 15:40:05 +0300

    Scripted auto-commit on change (2020-10-23 15:40:05) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -63,10 +63,10 @@ public class Main {
                     return new Tuple2<>(maxDelay, (count * 100f) / size);
                 })
                 .map(data ->{
-//                    String originAirportName = airportsBroadcasted.value().get(data._1._1).getName();
-//                    String destAirportName = airportsBroadcasted.value().get(data._1._2).getName();
-                    String originAirportName = m.get(data._1._1).getName();
-                    String destAirportName = m.get(data._1._2).getName();
+                    String originAirportName = airportsBroadcasted.value().get(data._1._1).getName();
+                    String destAirportName = airportsBroadcasted.value().get(data._1._2).getName();
+//                    String originAirportName = m.get(data._1._1).getName();
+//                    String destAirportName = m.get(data._1._2).getName();
                     return new Tuple2<>(
                             new Tuple2<>(originAirportName, destAirportName),
                             data._2()
